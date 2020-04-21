a95872f66bcb76ddf0b53ec82277e74ba31b0543
Josh Jacobsen <joshj@entouch.net>
2020-04-21 01:20:18 -0500

    Completed Part 2

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    """The home page for Learning Log."""
+    return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """The home page for Topics List."""
+    return render(request, 'learning_logs/topics.html')
