1541bdbd20365f5820eb0d2548d24d4979566986
Josh Jacobsen <joshj@entouch.net>
2020-04-27 15:18:19 -0500

    Added Form for a New Topic

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Topic # Just added
+from django.shortcuts import render, redirect
+from .models import Topic
+from .forms import TopicForm
 # Create your views here.
 
 def index(request):
@@ -16,5 +17,15 @@ def topic(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
     entries = topic.entry_set.order_by('-date_added') #-date... is descending order
     context = {'topic':topic, 'entries':entries}
-
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
